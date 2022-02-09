b876efa9a8b9e2bcab888c69c09ce9c10b53f0dc
Isabellukk <isabellukk@gmail.com>
2022-02-09 05:57:20 -0800

    cleaned up code

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,133 +1,112 @@
-import React, { useRef, useEffect, useState, setState, useSelector } from 'react';
+import React, {useRef, useEffect, useState, setState, useSelector} from 'react';
+import {btns, BTN_ACTIONS} from './btnConfig';
 import './calculator.css';
-import { btns, BTN_ACTIONS } from './btnConfig';
-
-const Calculator = () => {
-
-    const btnsRef = useRef(null);
-    const expRef = useRef(null);
-
-    const [expression, setExpression] = useState('');
 
 
+function Calculator() {
 
-    useEffect(() => {
-        const btns = Array.from(btnsRef.current.querySelectorAll('button'));
-        btns.forEach(e => e.style.height = e.offsetWidth + 'px');
-    }, []);
+  const btnsRef = useRef(null);
+  const expRef = useRef(null);
 
-    const btnClick = (item) => {
+  const [expression, setExpression] = useState('');
 
-        const curDiv = expRef.current;
+  useEffect(() => {
+    const btns = Array.from(btnsRef.current.querySelectorAll('button'));
+    btns.forEach(e => e.style.height = e.offsetWidth + 'px');
+  }, []);
 
+  const btnClick = (item) => {
 
-        if (item.action === BTN_ACTIONS.THEME) document.body.classList.toggle('dark');
+    const curDiv = expRef.current;
 
-        if (item.action === BTN_ACTIONS.ADD) {
+    if (item.action === BTN_ACTIONS.THEME)
+      document.body.classList.toggle('dark');
 
-            addAnimSpan(item.display);
-            const oper = item.display !== 'x' ? item.display : '*';
+    if (item.action === BTN_ACTIONS.ADD) {
 
-            setExpression(expression + oper)
+      addAnimSpan(item.display);
+      const oper = item.display !== 'x'
+        ? item.display
+        : '*';
 
-        }
-
-        if (item.action === BTN_ACTIONS.DELETE) {
-            curDiv.parentNode.querySelector('div:last-child').innerHTML = '';
-            curDiv.innerHTML = '';
-
-            setExpression('');
-        }
-
-        if (item.active && item.action === BTN_ACTIONS.ADD) {
-            return null;
-        }
-
-
-        if (item.action === BTN_ACTIONS.CALC) {
-            if (expression.trim().length <= 0) return;
-
-
-            curDiv.parentNode.querySelector('div:last-child').remove();
-
-
-            const cloneNode = curDiv.cloneNode(true);
-            curDiv.parentNode.appendChild(cloneNode);
-
-            const transform = `translateY(${-(curDiv.offsetHeight + 10) + 'px'}) scale(0.4)`;
-
-            try {
-                let res = eval(expression)
-
-                setExpression(res.toString());
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    curDiv.innerHTML = '';
-                    addAnimSpan(Math.floor(res * 100000000) / 100000000);
-                }, 200);
-            } catch (error) {
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    cloneNode.innerHTML = 'num num err';
-
-                }, 200
-
-
-
-
-              );
-            } finally {
-                console.log('Count Calcula Done');
-            }
-        }
-
-
-
-
-
-
+      setExpression(expression + oper)
 
     }
 
-    const addAnimSpan = (content) => {
-        const curDiv = expRef.current;
-        const span = document.createElement('span');
+    if (item.action === BTN_ACTIONS.DELETE) {
+      curDiv.parentNode.querySelector('div:last-child').innerHTML = '';
+      curDiv.innerHTML = '';
 
-        span.innerHTML = content;
-        span.style.opacity = '0';
-        curDiv.appendChild(span);
+      setExpression('');
+    }
 
-        const width = span.offsetWidth + 'px';
-        span.style.width = '0';
+    if (item.active && item.action === BTN_ACTIONS.ADD) {
+      return null;
+    }
 
+    if (item.action === BTN_ACTIONS.CALC) {
+      if (expression.trim().length <= 0)
+        return;
+
+      curDiv.parentNode.querySelector('div:last-child').remove();
+
+      const cloneNode = curDiv.cloneNode(true);
+      curDiv.parentNode.appendChild(cloneNode);
+
+      const transform = `translateY(${ - (curDiv.offsetHeight + 10) + 'px'}) scale(0.4)`;
+
+      try {
+        let res = eval(expression)
+
+        setExpression(res.toString());
         setTimeout(() => {
-            span.style.opacity = '1';
-            span.style.width = width;
-        }, 100);
+          cloneNode.style.transform = transform;
+          curDiv.innerHTML = '';
+          addAnimSpan(Math.floor(res * 100000000) / 100000000);
+        }, 200);
+      } catch (error) {
+        setTimeout(() => {
+          cloneNode.style.transform = transform;
+          cloneNode.innerHTML = 'num num err';
+
+        }, 200);
+      } finally {
+        console.log('Count Calcula Done');
+      }
     }
 
-    return (
-        <div className="calculator">
-            <div className="calculator__result">
-                <div ref={expRef} className="calculator__result__exp"></div>
-                <div className="calculator__result__exp"></div>
-            </div>
-            <div ref={btnsRef} className="calculator__btns">
-                {
-                    btns.map((item, index) => (
-                        <button
-                            key={index}
-                            className={item.class}
-                            onClick={() => btnClick(item)}
-                        >
-                            {item.display}
-                        </button>
+  }
 
-                    ))
-                }
-            </div>
-        </div>
-    );
+  const addAnimSpan = (content) => {
+    const curDiv = expRef.current;
+    const span = document.createElement('span');
+
+    span.innerHTML = content;
+    span.style.opacity = '0';
+    curDiv.appendChild(span);
+
+    const width = span.offsetWidth + 'px';
+    span.style.width = '0';
+
+    setTimeout(() => {
+      span.style.opacity = '1';
+      span.style.width = width;
+    }, 100);
+  }
+
+  return (<div className="calculator">
+    <div className="calculator__result">
+      <div ref={expRef} className="calculator__result__exp"></div>
+      <div className="calculator__result__exp"></div>
+    </div>
+    <div ref={btnsRef} className="calculator__btns">
+      {
+        btns.map((item, index) => (<button key={index} className={item.class} onClick={() => btnClick(item)}>
+          {item.display}
+        </button>))
+      }
+    </div>
+  </div>);
 }
 
 export default Calculator;
--- src/components/calculator/btnConfig.js
+++ src/components/calculator/btnConfig.js
@@ -1,126 +1,102 @@
-
-
 export const BTN_ACTIONS = {
-    ADD: 'ADD',
-    THEME: 'THEME',
-    CALC: 'CALC',
-    DELETE: 'DELETE',
-
+  ADD: 'ADD',
+  THEME: 'THEME',
+  CALC: 'CALC',
+  DELETE: 'DELETE'
 }
 
-
-
 export const btns = [
-    {
-        display: 'AC',
-        action: BTN_ACTIONS.DELETE,
-        class: 'btn__op'
-    },
-    {
-        display: '(',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol'
-    },
-    {
-        display: ')',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol'
-    },
-    {
-        display: '/',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol'
-    },
-    {
-        display: '7',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '8',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '9',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: 'x',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol'
-    },
-    {
-        display: '4',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '5',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '6',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '-',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol'
-    },
-    {
-        display: '1',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '2',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '3',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '+',
-        action: BTN_ACTIONS.ADD,
-        class: 'btn__op',
-        active: 'symbol',
-        reducers: {
-          decrement: (state) => {
-            state.value -= 1;
-          }
-        }
-    },
-    {
-        display: '🌜',
-        action: BTN_ACTIONS.THEME,
-        class: ''
-    },
-    {
-        display: '0',
-        action: BTN_ACTIONS.ADD,
-        class: ''
-    },
-    {
-        display: '.',
-        action: BTN_ACTIONS.ADD,
-        class: '',
-        active: 'symbol'
-    },
-    {
-        display: '=',
-        action: BTN_ACTIONS.CALC,
-        class: 'btn__op'
+  {
+    display: 'AC',
+    action: BTN_ACTIONS.DELETE,
+    class: 'btn__op'
+  }, {
+    display: '(',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol'
+  }, {
+    display: ')',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol'
+  }, {
+    display: '/',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol'
+  }, {
+    display: '7',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '8',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '9',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: 'x',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol'
+  }, {
+    display: '4',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '5',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '6',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '-',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol'
+  }, {
+    display: '1',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '2',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '3',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '+',
+    action: BTN_ACTIONS.ADD,
+    class: 'btn__op',
+    active: 'symbol',
+    reducers: {
+      decrement: (state) => {
+        state.value -= 1;
+      }
     }
+  }, {
+    display: '🌜',
+    action: BTN_ACTIONS.THEME,
+    class: ''
+  }, {
+    display: '0',
+    action: BTN_ACTIONS.ADD,
+    class: ''
+  }, {
+    display: '.',
+    action: BTN_ACTIONS.ADD,
+    class: '',
+    active: 'symbol'
+  }, {
+    display: '=',
+    action: BTN_ACTIONS.CALC,
+    class: 'btn__op'
+  }
 ];
