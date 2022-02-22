c4e2e9149c1a3a3c90c495e496f397248ddb4e68
Isabellukk <isabellukk@gmail.com>
2022-02-22 03:11:16 -0800

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import fangs from './img/fangs.png'
+import "./App.css";
+import fangs from "./img/fangs.png";
 
-import Calculator from './components/calculator/Calculator';
-
+import Calculator from "./components/calculator/Calculator";
 
 function App() {
   return (
     <div className="calc">
-      <div className="count"><a href="https://youtu.be/Ur_aDdofl_8">Count Calcula</a></div>
-      <div className="fangs"><img src={fangs} alt=""/></div>
-        <Calculator/>
+      <div className="count">
+        <a href="https://youtu.be/Ur_aDdofl_8">Count Calcula</a>
+      </div>
+      <div className="fangs">
+        <img src={fangs} alt="" />
+      </div>
+      <Calculator />
     </div>
-  )
+  );
 }
 
 export default App;
